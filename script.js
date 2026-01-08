a7de07ecfca153bbc8b88fdb9452a94ecfce47f6
nishitparekh54-source <nishitparekh54@gmail.com>
2026-01-08 10:38:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ function calculateDose() {
     let humanDose = document.getElementById("humanDose").value;
     let animalWeight = document.getElementById("animalWeight").value;
     let animalType = document.getElementById("animalType").value;
+     // Step 6.2: Human dose in mg/kg
+    let humanDoseMgKg = humanDose / 60;
     alert(
         "Human Dose: " + humanDose + " mg\n" +
-        "Animal Weight: " + animalWeight + " g\n" +
-        "Animal Type: " + animalType
+        "Human Dose (mg/kg): " + humanDoseMgKg.toFixed(2)
     );
 }
