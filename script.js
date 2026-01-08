a58f27d31c9493896c175c3d20d63ea76edd1b80
nishitparekh54-source <nishitparekh54@gmail.com>
2026-01-08 10:32:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function calculateDose() {
 
-    // Read input values
     let humanDose = document.getElementById("humanDose").value;
     let animalWeight = document.getElementById("animalWeight").value;
     let animalType = document.getElementById("animalType").value;
 
-    // Show values (testing step)
     alert(
         "Human Dose: " + humanDose + " mg\n" +
         "Animal Weight: " + animalWeight + " g\n" +
